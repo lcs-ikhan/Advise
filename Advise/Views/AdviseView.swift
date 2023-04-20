c511d42a92b384aaea5f8024e200a17f4704bf1d
Isaad Khan <IKhan@lcs.on.ca>
2023-04-20 13:33:16 -0400

    Re-organized and added view and model group

---
+++ Advise/Views/AdviseView.swift
@@ -0,0 +1,20 @@
+//
+//  AdviseView.swift
+//  Advise
+//
+//  Created by Isaad Khan on 2023-04-20.
+//
+
+import SwiftUI
+
+struct AdviseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdviseView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdviseView()
+    }
+}
