2f18629e2e1679f1ea0ad6bfa09a8c3316b0cd48
Isaad Khan <IKhan@lcs.on.ca>
2023-04-20 13:31:08 -0400

    Initial Commit

---
+++ Advise/AdviseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdviseApp.swift
+//  Advise
+//
+//  Created by Isaad Khan on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct AdviseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
